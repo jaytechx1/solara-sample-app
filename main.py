bad54056c594f4046ab0e31234167773cca745be
Jay Patel <153970755+jaytechx1@users.noreply.github.com>
2023-12-16 09:48:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
+# Test application, not be entered into production mode
+import numpy as np
+import plotly.express as px
 
+import solara
+
+x = np.linspace(0, 2, 100)
+
+title = "Interactive sine wave"
+freq = solara.reactive(2.0)
+phase = solara.reactive(0.1)
+
+
+@solara.component
+def Page():
+    y = np.sin(x * freq.value + phase.value)
+
+    solara.FloatSlider("Frequency", value=freq, min=0, max=10)
+    solara.FloatSlider("Phase", value=phase, min=0, max=np.pi, step=0.1)
+
+    fig = px.line(x=x, y=y)
+    solara.FigurePlotly(fig)
